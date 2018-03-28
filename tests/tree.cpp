b303a67eb6d9f29618f05469783d3ba3a21d5dfa
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2018-03-29 02:24:06 +0300

    Update tree.cpp

--- tests/tree.cpp
+++ tests/tree.cpp
@@ -8,3 +8,33 @@ TEST_CASE("creating tree")
 	tree_t<int> tree;
 	REQUIRE( tree.root() == nullptr );
 }
+
+TEST_CASE("insert")
+{
+	std::string input{
+		"        ----9\n"
+		"                ----8\n"
+		"            ----7\n"
+		"    ----7\n"
+		"        ----6\n"
+		"----5\n"
+		"        ----4\n"
+		"    ----4\n"
+		"        ----3\n"};
+	
+	tree_t<int> tree;
+	
+	tree.insert(5);
+	tree.insert(7);
+	tree.insert(4);
+	tree.insert(6);
+	tree.insert(9);
+	tree.insert(7);
+	tree.insert(8);
+	tree.insert(3);
+	tree.insert(4);
+	
+	std::ostringstream ostream;
+	tree.print(ostream);
+	REQUIRE( input == ostream.str() );
+}
