87f1638b9111742219050c4214b7359cde538797
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2018-03-29 01:22:53 +0300

    Update example.cpp

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <sstream>
+
+#include "tree.hpp"
+
+int main()
+{
+	bool success = true;
+	while (success) {
+		std::string stroka;
+		getline(std::cin, stroka);
+		std::istringstream istream(stroka);
+		success = D.read(istream);
+	}
+
+	std::cin.get();
+    return 0;
+}
 
