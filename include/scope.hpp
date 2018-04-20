cdd3d75515d0be936069239ea920df1b1f2645f6
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2018-04-20 15:28:52 +0300

    Update and rename tree.hpp to scope.hpp

---
+++ include/scope.hpp
@@ -0,0 +1,63 @@
+template <typename T>
+class scoped_ptr {
+	T * ptr_;
+	scoped_ptr(scoped_ptr<T> const & other) = delete;             // Запрет конструктора копирования
+	scoped_ptr & operator=(scoped_ptr<T> const & other) = delete; // Запрет копирующего присваивания 
+public:
+	scoped_ptr(T * ptr): ptr_(ptr){ }
+	
+	~scoped_ptr() {
+		if (ptr_) {
+			delete ptr_;
+		}
+	}
+
+	T * operator->() { 
+		if (ptr_) {
+			return ptr_;
+		}
+	}
+
+	T & operator* () { 
+		if (ptr_) {
+			return *ptr_;
+		}
+	}
+
+	T * get() const {
+		if (ptr_) {
+			return ptr_;
+		}
+	}
+
+	void reset(T * ptr);
+
+	bool isEmpty();
+
+	void swap(scoped_ptr & other);
+};
+
+template <typename T>
+void scoped_ptr<T>::swap(scoped_ptr & other) {
+	if (ptr_ || other.ptr_) {
+		T * buff = ptr_;
+		ptr_ = other.ptr_;
+		other.ptr_ = buff;
+	}
+}
+
+template <typename T>
+bool scoped_ptr<T>::isEmpty() {
+	if (ptr_) {
+		return false;
+	}
+	return true;
+}
+
+template <typename T>
+void scoped_ptr<T>::reset(T * ptr) {
+	if (ptr_) {
+		delete ptr_;
+		ptr_ = ptr;
+	}
+}
