95fb3acfbbe9dffa5d915e9081c4b70efc32f298
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2018-04-22 12:25:44 +0300

    Update scope.cpp

--- tests/scope.cpp
+++ tests/scope.cpp
@@ -3,156 +3,62 @@
 
 #include "tree.hpp"
 
-TEST_CASE("creating tree")
+TEST_CASE("creating scope_ptr and get ptr")
 {
-	tree_t<int> tree;
-	REQUIRE( tree.root() == nullptr );
+	int * p = new int;
+	scoped_ptr <int> scope ( p );
+	
+	REQUIRE( scope.get() == p );
 }
 
-TEST_CASE("insert and print tree")
+TEST_CASE("operator *")
 {
-	std::string input{
-		"        ----9\n"
-		"                ----8\n"
-		"            ----7\n"
-		"    ----7\n"
-		"        ----6\n"
-		"----5\n"
-		"        ----4\n"
-		"    ----4\n"
-		"        ----3\n"};
+	int * p = new double;
+	*p = 6.1;
+	scoped_ptr <double> scope ( p );
 	
-	tree_t<int> tree { 5, 7, 4, 6, 9, 7, 8, 3, 4 };
-	
-	std::ostringstream ostream;
-	tree.print(ostream);
-	REQUIRE( input == ostream.str() );
+	REQUIRE( *scope == 6.1 );
 }
 
-TEST_CASE("find tree")
+TEST_CASE("operator ->")
 {
-	tree_t<int> tree;
+	int * p = new int;
+	*p = 6;
+	scoped_ptr <int> scope ( p );
 	
-	tree.insert(5);
-	tree.insert(7);
-	tree.insert(4);
-	
-	REQUIRE( tree.find(5) == true );
-	REQUIRE( tree.find(7) == true );
-	REQUIRE( tree.find(0) == false );
+	REQUIRE( scope.operator->() == 6 );
 }
 
-TEST_CASE("remove tree")
+TEST_CASE("reset")
 {
-	tree_t<int> tree { 5, 7, 4 };
+	int * p = new int;
+	int * a = new int;
+	scoped_ptr<int> scope ( p );
 	
-	REQUIRE( tree.remove(6) == false );
-	REQUIRE( tree.remove(7) == true );
-	REQUIRE( tree.remove(5) == true );
-	REQUIRE( tree.remove(4) == true );
+	scope.reset(a);
+	
+	REQUIRE( scope.get() == a );
 }
 
-TEST_CASE("compare tree")
+TEST_CASE("swap")
 {
-	tree_t<int> A { 5, 7, 4 };
-	tree_t<int> B { 5, 7, 4 };
-	tree_t<int> C { 5, 7, 4, 4 };
+	int * p = new int;
+	int * a = new int;
+	scoped_ptr<int> scope1 ( p );
+	scoped_ptr<int> scope2 ( a );
 	
-	REQUIRE( (A == B) == true );
-	REQUIRE( (C == B) == false );
+	scope1.swap(scope2);
+	
+	REQUIRE( scope1.get() == a );
+	REQUIRE( scope2.get() == p );
 }
 
-TEST_CASE("read tree")
+TEST_CASE("isEmpty")
 {
-	tree_t<double> tree;
+	int * p = new int;
+	scoped_ptr<int> scope1 ( p );
+	scoped_ptr<int> scope2 ( nullptr );
 	
-	bool success = true;
-	for(std::size_t i = 0; i < 5; ++i) {
-		if (i == 0) {
-			std::string input{"+9.8"};
-			
-			std::istringstream istream(input);
-			std::ostringstream ostream;
-		
-			success = tree.read(istream, ostream);
-			REQUIRE( success == true );
-		}
-		else if (i == 1) {
-			std::string input{"="};
-			std::string output{"----9.8\n"};
-			std::istringstream istream(input);
-			std::ostringstream ostream;
-		
-			success = tree.read(istream, ostream);
-			REQUIRE( success == true );
-			REQUIRE( output == ostream.str() );
-		}
-		else if (i == 2) {
-			std::string input{"?1"};
-			std::string output{"false"};
-			std::istringstream istream(input);
-			std::ostringstream ostream;
-			
-			success = tree.read(istream, ostream);
-			REQUIRE( success == true );
-		}
-		else if (i == 3) {
-			std::string input{"d"};
-			std::istringstream istream(input);
-			std::ostringstream ostream;
-		
-			success = tree.read(istream, ostream);
-			REQUIRE( success == true );
-		}
-		else if (i == 4) {
-			std::string input{"q"};
-			std::istringstream istream(input);
-			std::ostringstream ostream;
-		
-			success = tree.read(istream, ostream);
-			REQUIRE( success == false );
-		}
-	}
-}
-
-SCENARIO("BST delete non inserted element", "[delete]") {
-    tree_t<int> tree = {8};
-    REQUIRE( !tree.remove(4) );
-    REQUIRE( !tree.isEmpty() );
-}
-
-SCENARIO("BST delete root without children", "[delete]") {
-    tree_t<int> tree = {8};
-    REQUIRE( tree.remove(8) );
-    REQUIRE( tree.isEmpty() );
-}
-
-SCENARIO("BST delete root with one child", "[delete]") {
-    tree_t<int> tree = {8, 4, 3};
-    REQUIRE( tree.remove(8) );
-    REQUIRE( tree == tree_t<int>({4, 3}) );
-}
-
-SCENARIO("BST delete root with children", "[delete]") {
-    tree_t<int> tree = {8, 4, 3, 10, 9, 13, 11, 12};
-    REQUIRE( tree.remove(8) );
-    REQUIRE( tree == tree_t<int>({10, 9, 4, 3, 13, 11, 12}) );
-}
-
-SCENARIO("BST delete non root without children", "[delete]") {
-    tree_t<int> tree = {8, 4, 3, 10, 9, 13, 11, 12};
-    REQUIRE( tree.remove(3) );
-    REQUIRE( tree == tree_t<int>({8, 4, 10, 9, 13, 11, 12}) );
-}
-
-SCENARIO("BST delete non root with one child", "[delete]") {
-    tree_t<int> tree = {8, 4, 3, 10, 9, 13, 11, 12};
-    REQUIRE( tree.remove(11) );
-    REQUIRE( tree == tree_t<int>({8, 4, 3, 10, 9, 13, 12}) );
-}
-
-SCENARIO("BST delete non root with children", "[delete]") {
-    tree_t<int> tree = {8, 4, 3, 10, 9, 13, 11, 12};
-    REQUIRE( tree.remove(10) );
-    REQUIRE( tree == tree_t<int>({8, 4, 3, 13, 11, 12, 9}) );
+	REQUIRE( scope1.isEmpty() == false );
+	REQUIRE( scope2.isEmpty() == true );
 }
